4afa238627d8220c1b9f5de054fb7bb0fb1ab204
Milogav <milogav.m77@gmail.com>
2019-06-04 19:58:50 +0200

    added speech to text conversion (through google speech-to-text Web API) to audioData class

--- audioDevice.py
+++ audioDevice.py
@@ -4,6 +4,10 @@ import numpy as np
 from subprocess import call,DEVNULL,getoutput
 from scipy.io import wavfile
 import os
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
+import re 
 
 def printInPlace(text):
     print('\r'+text+'\t'*5,end='',sep = '')
@@ -17,7 +21,7 @@ class audioData:
         if from_filepath is not None and os.path.isfile(from_filepath):
             self.read(from_filepath)
     
-    def save(self,filepath,bitrate = '322k'):
+    def save(self,filepath,bitrate = '322k',outFormat = 'mp3'):
         auxPath = filepath.split('.')[0] + '.wav'
         wavfile.write(auxPath,self.rate,self.data)
         mp3ConvCommand = ['ffmpeg','-y','-i',auxPath,'-vn','-ar',str(self.rate),'-ac',str(self.channels),'-b:a',bitrate,filepath]
@@ -34,6 +38,58 @@ class audioData:
         _,self.data = wavfile.read(auxPath)
         self.channels = len(self.data.shape)
         os.remove(auxPath)
+    
+    def toText(self,language = 'es-ES'):
+        
+        auxPath = 'audioSample.wav'
+        flacPath = 'audioSample.flac'
+        sample = self.data
+        if self.channels == 2:
+             sample = (sample[:,0] + sample[:,1] ) /2
+             sample = sample.astype(np.int16)
+
+        wavfile.write(auxPath,self.rate,sample)
+        flacConvCommand = ['ffmpeg','-y','-i',auxPath,'-c:a','flac',flacPath]
+        call(flacConvCommand, stderr=DEVNULL, stdout=DEVNULL) 
+        with open(flacPath,'rb') as f:
+            flacBytes = f.read()
+        
+        ############# CODE LINES FROM: https://github.com/Uberi/speech_recognition/blob/master/speech_recognition/__init__.py
+        key = "AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw"   ###### google speech recognition API key
+        url = "http://www.google.com/speech-api/v2/recognize?{}".format(urlencode({
+            "client": "chromium",
+            "lang": language,
+            "key": key,
+            "pFilter": 0
+        }))
+        request = Request(url, data=flacBytes, headers={"Content-Type": 'audio/x-flac; rate=%d' % self.rate})
+    
+        os.remove(auxPath)
+        os.remove(flacPath)
+
+        try:
+            response = urlopen(request, timeout=5)
+        except HTTPError as e:
+            print('Recognition request failed!!!')
+        except URLError as e:
+            print('Recognition connection failed!!!')
+        response_text = response.read().decode("utf-8")
+
+        ##### parse response text
+        speech = ''
+        results = re.findall('transcript":"(.*)","',response_text)
+        confidences = re.findall('confidence":(.*)}',response_text)
+
+        if not results:
+            speech = '*error*'
+        elif confidences is None or len(results) < 2:
+            speech = results[0]
+        else:
+            confidences = [float(x) for x in confidences]
+            bestIdx = np.argmax(confidences)
+            speech = results[bestIdx]
+
+        return speech
 
 
 class audioDevice:
@@ -85,7 +141,7 @@ class audioDevice:
         recording = audioData(from_dataArray = recAudio,channels =  self.channels,rate = self.rate,fmt = self.fmt)
         return recording
     
-    def recordOnSound(self,f_thr = 1.25,silenceStopTime = 1):
+    def recordOnSound(self,f_thr = 3,silenceStopTime = 1):
         silence = True
         stream = self.audio.open(format = self.fmt,
                             channels = self.channels,
@@ -140,16 +196,18 @@ class audioDevice:
 
 
 
-AD = audioDevice()
+AD = audioDevice(rate=44100)
 
-recording = AD.recordOnSound()
-
+while True:
+    recording = AD.recordOnSound()
+    speech = recording.toText()
+    print(speech)
 # recording = AD.recordTime(recTimeSeconds = 60)
 # recording.save('/home/miguel/audio.mp3')
 
 # recLoad = audioData(from_filepath='/home/miguel/audio.mp3')
 
-AD.playAudio(recording)
+# AD.playAudio(recording)
 # waveFile = wave.open('file.mp3', 'wb')
 # waveFile.setnchannels(AR.channels)
 # waveFile.setsampwidth(AR.audio.get_sample_size(AR.fmt))
